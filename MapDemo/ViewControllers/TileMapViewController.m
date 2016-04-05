220f41df49da6ccf08d83422b008e1acd3f7410a
Troy Zhang <java.koma@gmail.com>
2016-04-05 11:03:47 +0800

    update project

--- MapDemo/ViewControllers/TileMapViewController.m
+++ MapDemo/ViewControllers/TileMapViewController.m
@@ -18,7 +18,7 @@
 // 如果距离小于200米，则播报
 #define MAX_NEAR_BY_DISTANCE                200
 
-@interface TileMapViewController ()<ISSPinAnnotationMapViewDelegate, BDSSpeechSynthesizerDelegate>
+@interface TileMapViewController ()<ISSPinAnnotationMapViewDelegate, NAMapViewDelegate, BDSSpeechSynthesizerDelegate>
 {
     ISSPinAnnotationMapView *mapView;
     UIButton *soundBtnView;
@@ -32,9 +32,12 @@
     BOOL isSoundAutoPlayDisabled;
 #ifdef IS_MOCK_NAVIGATION
     UIButton *mockNavBtnView;
-//    NSMutableArray *mockLocList;
     NSInteger mockIndex;
 #endif
+    
+    // zoom
+    UIButton *zoomInBtnView;
+    UIButton *zoomOutBtnView;
 }
 @end
 
@@ -80,8 +83,6 @@
 {
     lastNearIndex = -1;
     
-//    mapItem = [[MapItemModel arrayOfModelsFromData:[FileUtil readFileFromPath:@"yugudong.json"] error:nil] objectAtIndex:0];
-    
     leftTopCoor = BMKMapPointForCoordinate(CLLocationCoordinate2DMake(mapItem.topLeftLat, mapItem.topLeftLng));
     
     if([mapItem hasTiles])
@@ -118,8 +119,6 @@
     
 #ifdef IS_MOCK_NAVIGATION
     mockIndex = -1;
-//    mockLocList = [[NSMutableArray alloc] init];
-//    mockLocList = [UserLocationModel arrayOfModelsFromData:[FileUtil readFileFromPath:@"mock_location_list.json"] error:nil];
 #endif
 }
 
@@ -127,6 +126,7 @@
 {
     [self initMapView];
     [self initSoundControlView];
+    [self initZoomButtonView];
 #ifdef IS_MOCK_NAVIGATION
     [self initMocButtonView];
 #endif
@@ -152,9 +152,10 @@
         mapView.minimumZoomScale = mapView.minimumZoomScale < mapView.zoomScale ? mapView.zoomScale : mapView.minimumZoomScale;
     }
     mapView.mapDelegate = self;
+    mapView.mapViewDelegate = self;
     mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     
-    mapView.zoomStep = 3.0f;
+    mapView.zoomStep = 0.2f;
     
     [self.view addSubview:mapView];
     
@@ -178,6 +179,32 @@
     [soundBtnView setImage:[UIImage imageNamed:@"btn_sound_nomal.png"] forState:UIControlStateSelected];
     [soundBtnView addTarget:self action:@selector(soundButtonTapped) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:soundBtnView];
+}
+
+- (void)initZoomButtonView
+{
+    zoomInBtnView = [UIButton buttonWithType:UIButtonTypeCustom];
+    UIImage *zoomInImage = [UIImage imageNamed:@"zoomin_normal"];
+    UIImage *zoomInDisabledImage = [UIImage imageNamed:@"zoomin_yes"];
+    [zoomInBtnView setBackgroundImage:zoomInImage forState:UIControlStateNormal];
+    [zoomInBtnView setBackgroundImage:zoomInDisabledImage forState:UIControlStateDisabled];
+    
+    zoomOutBtnView = [UIButton buttonWithType:UIButtonTypeCustom];
+    UIImage *zoomOutImage = [UIImage imageNamed:@"zoomout_normal"];
+    UIImage *zoomOutDisabledImage = [UIImage imageNamed:@"zoomout_yes"];
+    [zoomOutBtnView setBackgroundImage:zoomOutImage forState:UIControlStateNormal];
+    [zoomOutBtnView setBackgroundImage:zoomOutDisabledImage forState:UIControlStateDisabled];
+    
+    zoomOutBtnView.frame = CGRectMake(kScreenWidth - zoomOutImage.size.width - 8, kScreenHeight - zoomOutImage.size.height - 8, zoomOutImage.size.width, zoomOutImage.size.height);
+    zoomInBtnView.frame = CGRectMake(zoomOutBtnView.frame.origin.x, zoomOutBtnView.frame.origin.y - zoomInImage.size.height, zoomInImage.size.width, zoomInImage.size.height);
+    
+    [self.view addSubview:zoomInBtnView];
+    [self.view addSubview:zoomOutBtnView];
+    
+    [zoomInBtnView addTarget:self action:@selector(zoomInButtonTapped) forControlEvents:UIControlEventTouchUpInside];
+    [zoomOutBtnView addTarget:self action:@selector(zoomOutButtonTapped) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self updateZoomButtonStatus];
 }
 
 - (void)initTTSConfig
@@ -466,6 +493,38 @@
     }
 }
 #endif
+
+#pragma mark zoom in/out
+- (void)zoomInButtonTapped
+{
+    if(mapView.zoomScale < mapView.maximumZoomScale)
+    {
+        CGFloat newZoomScale = mapView.zoomScale + mapView.zoomStep;
+        newZoomScale = newZoomScale > mapView.maximumZoomScale ? mapView.maximumZoomScale : newZoomScale;
+        [mapView setZoomScale:newZoomScale animated:YES];
+    }
+}
+
+- (void)zoomOutButtonTapped
+{
+    if(mapView.zoomScale > mapView.minimumZoomScale)
+    {
+        CGFloat newZoomScale = mapView.zoomScale - mapView.zoomStep;
+        newZoomScale = newZoomScale < mapView.minimumZoomScale ? mapView.minimumZoomScale : newZoomScale;
+        [mapView setZoomScale:newZoomScale animated:YES];
+    }
+}
+
+- (void)mapView:(NAMapView *)mapView hasChangedZoomLevel:(CGFloat)level
+{
+    [self updateZoomButtonStatus];
+}
+
+- (void)updateZoomButtonStatus
+{
+    [zoomInBtnView setEnabled:(mapView.zoomScale < mapView.maximumZoomScale)];
+    [zoomOutBtnView setEnabled:(mapView.zoomScale > mapView.minimumZoomScale)];
+}
 
 - (void)dealloc
 {
